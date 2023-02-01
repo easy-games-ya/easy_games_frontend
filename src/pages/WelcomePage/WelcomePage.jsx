b934a2768345e78144505aee5fbfb7c76b0a1b4a
Aleksey Shemyakin <carl0slit0s@yndex.ru>
2023-02-02 01:56:37 +0500

    structure welcome page

---
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './WelcomePage.css';
+
+const WelcomePage = () => {
+  return (
+    <main className='welcome-page'>
+      {/* logo */}
+      <h1>Добро пожаловать</h1>
+      {/* form */}
+      {/* правила */}
+    </main>
+  );
+};
+
+export default WelcomePage;
