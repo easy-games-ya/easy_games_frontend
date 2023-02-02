dc2ba6d5c4fcebc84f143bd677cdde27c85511d6
Artyom <artyom-first@yandex.ru>
2023-02-02 21:23:10 +0500

    feat: сверстать profile-card

--- src/games/ItGame/pages/MainPage/MainPage.jsx
+++ src/games/ItGame/pages/MainPage/MainPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './MainPage.css';
 import Header from '../../components/Header/Header';
+import InfoSection from '../../components/InfoSection/InfoSection';
 
 const MainPage = () => {
   return (
@@ -10,31 +11,7 @@ const MainPage = () => {
         <section className='grid-section'>
           <div className='grid'>Игра-викторина &quot;Своя игра&quot;</div>
         </section>
-        <section className='info-section'>
-          <div className='profile-card'>
-            <img className='profile-card__avatar' src='' alt='Аватарка' />
-            <p className='profile-card__name'>Иероним</p>
-            <p className='profile-card__score'>1250</p>
-          </div>
-          <div className='records'>
-            <p className='recards__title'>Лучшие результаты</p>
-            <ul className='records__flex-row'>
-              <li className='record-card'>
-                <p className='record-card__name'>Леонардо</p>
-                <p className='record-card__score'>4500</p>
-              </li>
-              <li className='record-card'>
-                <p className='record-card__name'>Донателло</p>
-                <p className='record-card__score'>2300</p>
-              </li>
-              <li className='record-card'>
-                <p className='record-card__name'>Рафаэль</p>
-                <p className='record-card__score'>3300</p>
-              </li>
-            </ul>
-          </div>
-          <p className='info-section__time'>00:30</p>
-        </section>
+        <InfoSection />
       </main>
     </div>
   );
