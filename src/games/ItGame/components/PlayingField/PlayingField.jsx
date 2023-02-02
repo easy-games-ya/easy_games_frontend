e88a16fc324d71cd077d6046eb9fab900e0d011f
Artyom <artyom-first@yandex.ru>
2023-02-02 21:55:01 +0500

    feat: сверстать the InfoSection component for MainPage in ItGame for 1920px

---
+++ src/games/ItGame/components/PlayingField/PlayingField.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './PlayingField.css';
+
+const PlayingField = () => {
+  return (
+    <section className='playing-field'>
+      <div className='grid'>Игра-викторина &quot;Своя игра&quot;</div>
+    </section>
+  );
+};
+
+export default PlayingField;
