70dd008d8b10ca0ae7688619adbc04d4704a0e34
Aleksey Shemyakin <carl0slit0s@yndex.ru>
2023-02-05 00:01:21 +0500

    модальное окно с правилами

---
+++ src/games/ItGame/components/ModalRules/ModalRules.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './ModalRules.css';
+
+const ModalRules = () => {
+  return (
+    <div className='modal-rules'>
+      <div className='modal-container'>
+        <h3 className='modal__title'>Праила игры</h3>
+        <ol>
+          <li className='modal-rules__text'>Играть</li>
+          <li className='modal-rules__text'>Кнопку нажимать</li>
+          <li className='modal-rules__text'>Отвечать</li>
+          <li className='modal-rules__text'>Выигрывать/ проигрывать</li>
+          <li className='modal-rules__text'>Радоваться/ не расстраиваться</li>
+
+        </ol>
+      </div>
+    </div>
+  );
+};
+
+export default ModalRules;
