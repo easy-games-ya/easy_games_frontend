59228fd115203fbcab5792d4c197b489b60c5d31
Aleksey Shemyakin <carl0slit0s@yndex.ru>
2023-02-06 01:32:21 +0500

    добавить модификатор текста в кнопку

--- src/games/ItGame/ui/button.jsx
+++ src/games/ItGame/ui/button.jsx
@@ -1,17 +1,15 @@
 import React from 'react';
 import './button.css';
+import TextYellowFirstLetter from './TextYellowFirstLetter/TextYellowFirstLetter';
 
 const Button = ({ isSubmit, isDisabled, text }) => {
-  const letters = text.split('');
-  letters[0] = <span style={{ color: 'var(--color-text-accent)' }}>{letters[0]}</span>;
-
   return (
     <button
       className='button'
       type={isSubmit ? 'submit' : 'button'}
       disabled={isDisabled}
     >
-      {letters}
+      {TextYellowFirstLetter({ text })}
     </button>
   );
 };
