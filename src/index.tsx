e8c3fc6160a3a438f3c0af858c2bc6f383b9bd82
Artyom <artyom-first@yandex.ru>
2023-02-21 23:47:32 +0500

    feat: initialize TypeScript

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,9 @@ import './index.css';
 import App from './components/App/App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
 root.render(
   <React.StrictMode>
     <BrowserRouter>
