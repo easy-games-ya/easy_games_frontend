14477d917969586c81ab1223477bb7ae10726efe
Artyom <artyom-first@yandex.ru>
2023-02-22 00:27:00 +0500

    fix: изменить настройки extends

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -5,11 +5,13 @@ import ProtectedRoute from '../ProtectedRoute';
 import MainPage from '../../pages/MainPage/MainPage';
 import NotFoundPage from '../../pages/NotFoundPage/NotFoundPage';
 
-import ItGame from '../../games/ItGame/index';
-import MainPageItGame from '../../games/ItGame/pages/MainPage/MainPage';
-import WelcomePageItGame from '../../games/ItGame/pages/WelcomePage/WelcomePage';
-import QuestionPageItGame from '../../games/ItGame/pages/QuestionPage/QuestionPage';
-import FinalPageItGame from '../../games/ItGame/pages/FinalPage/FinalPage';
+import ItGame from '../../games/ItGame';
+import {
+  MainPageItGame,
+  WelcomePageItGame,
+  QuestionPageItGame,
+  FinalPageItGame,
+} from '../../games/ItGame/pages';
 
 const App = () => {
   // eslint-disable-next-line no-unused-vars
@@ -24,7 +26,7 @@ const App = () => {
         <Route path='question/:id' element={<QuestionPageItGame />} />
         <Route path='final' element={<FinalPageItGame />} />
       </Route>
-      <Route path='/easy_games_frontend' element={<Navigate to='/it-game' replace />} />
+      <Route path='/easy_games_frontend' element={<Navigate to='/' replace />} />
       <Route path='*' element={<NotFoundPage />} />
     </Routes>
   );
